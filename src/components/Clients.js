e3d0492c477eec145f3e520127024d62c3e8dd52
Ankit Chauhan <ankit.chauhan@unifize.com>
2018-12-13 22:46:01 +0530

    Logos added

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -3,19 +3,24 @@ import styled from 'styled-components'
 
 import bookbyslot from './img/clients/bookbyslot.png'
 import looksart from './img/clients/looksart.png'
-import luvstay from './img/clients/luvstay.png'
+import luvstay from './img/clients/luvstay2.png'
 import navavarn from './img/clients/navavarn.jpg'
 import openapp from './img/clients/openapp.png'
-import solarchs from './img/clients/solarchs.png'
 import tourstud from './img/clients/tourstud.png'
 import unboxx from './img/clients/unboxx.png'
+import saral from './img/clients/saral.jpg'
+import koushik from './img/clients/koushik.png'
+import nutriplate from './img/clients/nutriplate.jpg'
+import microsoft from './img/clients/microsoft.png'
+import careercafe from './img/clients/careercafe.webp'
 
 const Clients = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
   grid-gap: 2rem;
   margin: 0 1rem;
   img {
+    margin: auto 0;
     width: 100%;
   }
   @media (max-width: 800px) {
@@ -27,12 +32,15 @@ class ClientSection extends Component {
   render() {
     return (
       <Clients>
-        <img src={bookbyslot} />
+        <img src={microsoft} />
+        <img src={bookbyslot} style={{ background: 'grey' }} />
         <img src={looksart} />
         <img src={luvstay} />
         <img src={navavarn} />
         <img src={openapp} />
-        <img src={solarchs} />
+        <img src={saral} />
+        <img src={nutriplate} />
+        <img src={careercafe} />
         <img src={tourstud} />
         <img src={unboxx} />
       </Clients>
