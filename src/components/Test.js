9dde0d897f973d8155daf412133eb04798ecf2ba
Ankit Chauhan <ankit.chauhan@unifize.com>
2018-12-28 16:02:24 +0530

    Form Submit Test

---
+++ src/components/Test.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Test extends Component {
+  componentDidMount() {
+    axios.get('/hello', res => console.log('I am here', res))
+  }
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>
+        <form name="test" netlify>
+          <label for="name">
+            <span>Name</span>
+            <input type="text" name="name" />
+          </label>
+          <label for="email">
+            <span>Email</span>
+            <input type="text" name="email" />
+          </label>
+          <label for="message">
+            <span>Message</span>
+            <input type="text" name="message" />
+          </label>
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Test
