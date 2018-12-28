7d499dd7008d3e34bc2b1e93354931a8f42a4596
Ankit Chauhan <ankit.chauhan@unifize.com>
2018-12-28 16:55:55 +0530

    Contact Form Update

--- src/components/Test.js
+++ src/components/Test.js
@@ -8,6 +8,13 @@ class Test extends Component {
         <h1>Test</h1>
         <form name="contact" method="post" data-netlify="true">
           <input type="text" name="name" id="name" placeholder="your name" />
+          <input type="text" name="email" id="email" placeholder="your email" />
+          <input
+            type="text"
+            name="message"
+            id="message"
+            placeholder="your message"
+          />
 
           <button>Submit</button>
         </form>
