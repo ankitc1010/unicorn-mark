f24c0f0d552c9fd6e70e547b50a4ced7278c523e
Ankit Chauhan <ankit.chauhan@unifize.com>
2018-12-28 16:49:33 +0530

    Netlify form

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class Test extends Component {
-  componentDidMount() {
-    axios.get('/hello', res => console.log('I am here', res))
-  }
   render() {
     return (
       <div>
         <h1>Test</h1>
         <form name="contact" method="post" data-netlify="true">
-          <input type="text" name="name" placeholder="your name" />
+          <input type="text" name="name" id="name" placeholder="your name" />
 
           <button type="submit">Submit</button>
         </form>
