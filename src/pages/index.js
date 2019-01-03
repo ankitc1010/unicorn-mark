69990f517d45aa553b6e86aa5a46b39010981ae0
Ankit Chauhan <ankit.chauhan@unifize.com>
2019-01-04 00:59:17 +0530

    Seminars page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -306,6 +306,46 @@ class IndexPage extends React.Component {
                 </div>
               </div>
             </div>
+            <div className="row">
+              <div className="col-1-of-3">
+                <div className="card">
+                  <div className="card__side card__side--front">
+                    <div className="card__picture card__picture--2">&nbsp;</div>
+                    <h4
+                      className="card__heading"
+                      style={{ fontSize: '2.1rem' }}
+                    >
+                      <span className="card__heading-span card__heading-span--3">
+                        Seminars and Events
+                      </span>
+                    </h4>
+                    <div className="card__details">
+                      <ul>
+                        We conduct events and seminars to train and educate
+                        people about the latest trends that are there in the
+                        world right now.
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="card__side card__side--back card__side--back-3">
+                    <div className="card__cta">
+                      <div className="card__price-box">
+                        <p className="card__price-only">Conduct</p>
+                        <p className="card__price-value">Seminars and Events</p>
+                      </div>
+                      <Link
+                        to="/seminars/"
+                        activeStyle={{
+                          color: 'red',
+                        }}
+                      >
+                        <a className="btn btn--white">Tell me more</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
             {/* <div className="u-center-text u-margin-top-huge">
               <a href="#" className="btn btn--green">
---
+++ src/pages/seminars.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import Helmet from 'react-helmet'
+import image from '../assets/img/machinelearning.png'
+
+import '../assets/scss/main.scss'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+import { Wrapper, Page, Card } from '../components/styled/PageRelated'
+import { ContactButton } from '../components/Contact'
+
+const WebsitePage = () => (
+  <Wrapper>
+    <Nav />
+    <Helmet
+      title="Proof of Concept Development - Machine Learning Applications"
+      meta={[
+        {
+          name: 'description',
+          content: 'Seminars and Events',
+        },
+        {
+          name: 'keywords',
+          content:
+            'marketing, strategy consulting, consulting, web development, development, branding, machine learning, pocs',
+        },
+      ]}
+    />
+    <Page>
+      <div className="main">
+        <h1>Seminars and Events</h1>
+        <p>
+          Machine Learning Proof-of-Concepts are custom-built prototype
+          applications which demonstrate and measure the scope of automation and
+          AI for your business
+        </p>
+        <img src={image} />
+        <p>
+          These prototypes help prioritize investments in AI and Machine
+          Learning for companies based on a clear ROI with demonstrable
+          applications.
+        </p>
+        <p>
+          <ol>
+            <li>Organization Audit for AI Readiness</li>
+            <li>Customer Convenience AI</li>
+            <li>Customer Interaction AI</li>
+            <li>Business Intelligence AI</li>
+            <li>Internal Process Optimization AI</li>
+          </ol>
+        </p>
+        <p>
+          <ContactButton />
+        </p>
+      </div>
+      <div className="side">
+        <Card>
+          <h2> Read More</h2>
+          <div>
+            <div>
+              <span>1</span>
+              <div>
+                <p>Patient Health Monitoring</p>
+                <p>A healthcare AI startup</p>
+              </div>
+            </div>
+            <div>
+              <span>2</span>
+              <div>
+                <p>Business Intelligence Dashboard</p>
+                <p>A healthcare devices startup</p>
+              </div>
+            </div>
+          </div>
+        </Card>
+
+        <Card>
+          <h2>Case Studies</h2>
+          <div>
+            <div>
+              <span>1</span>
+              <div>
+                <p>Identify and Prioritize AI Impact (Coming Soon)</p>
+                <p>Building an AI roadmap for your business</p>
+              </div>
+            </div>
+            <div>
+              <span>2</span>
+              <div>
+                <p>
+                  Building an AI roadmap for your business Top Areas for AI
+                  Impact (Coming Soon)
+                </p>
+                <p>Customer-facing AI vs AI for Business Processes</p>
+              </div>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </Page>
+    <Footer />
+  </Wrapper>
+)
+
+export default WebsitePage
